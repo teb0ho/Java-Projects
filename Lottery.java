e25419633490b0736622a8dd48c241ee55401435
Teboho Mphure <tebohomphure@gmail.com>
2016-11-18 12:32:05 +0200

    Update Lottery.java

--- Lottery.java
+++ Lottery.java
@@ -13,57 +13,56 @@ public class Lottery {
 
 		System.out.println("Would you like to generate some numbers ? 1 = yes and 2 = no");
 
-		for (;;) {
+		try {
+			for (; ; ) {
 
-			Scanner scan = new Scanner(System.in);
+				Scanner scan = new Scanner(System.in);
 
-			int prompt = scan.nextInt();
+				int prompt = scan.nextInt();
 
-			if (prompt == 1) {
+				if (prompt == 1) {
 
-				BallHolder.add(null);
+					BallHolder.add(null);
 
-				for (int i = 0; i < 6; i++) {
+					for (int i = 0; i < 6; i++) {
 
-					gua = ballpicker.nextInt(50);
-					gua2 = ballpicker.nextInt(50);
+						gua = ballpicker.nextInt(50);
+						gua2 = ballpicker.nextInt(50);
 
-					if (!(BallHolder.contains(gua)) && gua > 0) {
+						if (!(BallHolder.contains(gua)) && gua > 0) {
 
-						BallHolder.add(gua);
+							BallHolder.add(gua);
 
-					} else if (!(BallHolder.contains(gua2)) && gua2 > 0) {
+						} else if (!(BallHolder.contains(gua2)) && gua2 > 0) {
 
-						BallHolder.add(gua2);
+							BallHolder.add(gua2);
 
-					} else {
+						}
+					}
+
+					for (int i = 1; i < BallHolder.size(); i++) {
+
+						System.out.print(BallHolder.get(i) + " ");
 
 					}
+
+					System.out.println("\nPress 2 to quit, 1 to generate new numbers\n");
+					BallHolder.clear();
+
+				} else if (prompt == 2) {
+
+					System.out.println("Thanks for running this program");
+					System.exit(0);
+
+				} else {
+
+					System.out.println("Incorrect option please try again.");
+
 				}
-
-				for (int i = 1; i < BallHolder.size(); i++) {
-
-					System.out.print(BallHolder.get(i) + " ");
-
-				}
-
-				System.out.println("\nPress 2 to quit, 1 to generate new numbers");
-				BallHolder.clear();
-
 			}
-
-			else if (prompt == 2) {
-
-				System.out.println("Thanks for running this program");
-				System.exit(0);
-
-			}
-
-			else {
-
-				System.out.println("Incorrect option please try again.");
-
-			}
+		}
+		catch (Exception e) {
+			System.out.println("Error - incorrect answer please enter the number 1 or 2 ");
 		}
 	}
 }
