a1bc6eb63886c6d1e1f26aaa2c97d5f90e8b7aca
Teboho Mphure <tebohomphure@gmail.com>
2016-12-06 12:46:39 +0200

    Update Lottery.java

--- src/Lottery.java
+++ src/Lottery.java
@@ -1,18 +1,120 @@
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Random;
 import java.util.Scanner;
 import java.util.ArrayList;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.File;
 import java.util.Date;
 
 /**
- * Created by Teb0ho on 2016-11-24 - 2016-11-28.
+ * Created by Teb0ho on 2016-11-24 - 2016-12-03.
  */
 
-public class Lottery {
+public class lotteryv2 {
+
+    String alphabet = "ABCDEFGHIJKLMNOPQRSTUVXWYZ";
+    String[] letters = new String[] {"A","A"};
+
+    int number = 0;
+    int number1 = -1;
+
+    String combination = letters[0] + number + letters[1] + number1;
+
+    void getNum() {
+        BufferedReader br =  null;
+        FileReader fr = null;
+        File file = new File("res/combos.txt").getAbsoluteFile();
+
+        try {
+            fr = new FileReader(file);
+            br = new BufferedReader(fr);
+
+            String sCurrentLine;
+            String sCurrentLine1 = "";
+
+            br = new BufferedReader(new FileReader(file));
+
+            while ((sCurrentLine = br.readLine()) != null) {
+                sCurrentLine1 = sCurrentLine;
+            }
+
+            number1 = Character.getNumericValue(sCurrentLine1.charAt(3));
+            letters[1] = String.valueOf(sCurrentLine1.charAt(2));
+            number = Character.getNumericValue(sCurrentLine1.charAt(1));
+            letters[0] = String.valueOf(sCurrentLine1.charAt(0));
+
+        }
+
+        catch (Exception e) {
+            System.out.println("1st order generated");
+        }
+
+        finally {
+            try {
+                if (br != null)
+                    br.close();
+                if (fr != null)
+                    fr.close();
+            }
+            catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    void genNums() {
+
+        if (!combination.contentEquals("Z9Z9")) {
+            number1++;
+
+            if (number1 > 9) {
+                int letterIndex = alphabet.indexOf(letters[1]) + 1;
+                if (letterIndex < alphabet.length()) {
+                    letters[1] = String.valueOf(alphabet.charAt(letterIndex));
+                }
+                else {
+                    number++;
+                    letters[1] = "A";
+                }
+                number1 = 0;
+            }
+
+            if (number > 9) {
+                int letterIndex = alphabet.indexOf(letters[0]) + 1;
+                if (letterIndex < alphabet.length()) {
+                    letters[0] = String.valueOf(alphabet.charAt(letterIndex));
+                }
+                number = 0;
+            }
+
+            combination = letters[0] + number + letters[1] + number1;
+        }
+
+
+        File file = new File("res/combos.txt").getAbsoluteFile();
+
+        try {
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+
+            FileWriter fileWriter = new FileWriter(file);
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+            bufferedWriter.write(combination);
+            bufferedWriter.close();
+            fileWriter.close();
+        }
+
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 
     static String lotto () {
 
@@ -100,8 +202,12 @@ public class Lottery {
         String textLotto;
         String textPwrBall;
         File file = new File("res/results.txt").getAbsoluteFile();
+        File file1 = new File("res/combos.txt");
         DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
         Date date = new Date();
+        lotteryv2 myLotObj = new lotteryv2();
+        BufferedReader br = null;
+        FileReader fr = null;
 
         System.out.println("Would you like to generate some numbers ? 1 = yes and 2 = no");
 
@@ -111,6 +217,8 @@ public class Lottery {
                 Scanner scan = new Scanner(System.in);
 
                 int prompt = scan.nextInt();
+                String sCurrentLine;
+                String sCurrentLine1 = "";
 
                 if (prompt == 1) {
 
@@ -119,8 +227,8 @@ public class Lottery {
 
                     if (lottoOpts == 1) {
                         textLotto = lotto();
-
-
+                        myLotObj.getNum();
+                        myLotObj.genNums();
 
                         if(!file.exists()) {
                             file.createNewFile();
@@ -128,7 +236,15 @@ public class Lottery {
 
                         FileWriter fileWriter = new FileWriter(file, true);
                         BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-                        bufferedWriter.write(textLotto + "\tLotto\t\t" + dateFormat.format(date) + "\n");
+                        fr = new FileReader(file1);
+                        br = new BufferedReader(fr);
+                        br = new BufferedReader(new FileReader(file1));
+
+                        while ((sCurrentLine = br.readLine()) != null) {
+                            sCurrentLine1 = sCurrentLine;
+                        }
+
+                        bufferedWriter.write(textLotto + "\tLotto\t\t" + dateFormat.format(date) + "\t" + sCurrentLine1 + "\n");
                         bufferedWriter.close();
 
                         System.out.println(textLotto);
@@ -136,6 +252,8 @@ public class Lottery {
 
                     else if (lottoOpts == 2) {
                         textPwrBall = powerBall();
+                        myLotObj.getNum();
+                        myLotObj.genNums();
 
                         if(!file.exists()) {
                             file.createNewFile();
@@ -143,7 +261,14 @@ public class Lottery {
 
                         FileWriter fileWriter = new FileWriter(file, true);
                         BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-                        bufferedWriter.write(textPwrBall + "\tPowerball\t" + dateFormat.format(date) + "\n");
+                        fr = new FileReader(file1);
+                        br = new BufferedReader(fr);
+                        br = new BufferedReader(new FileReader(file1));
+
+                        while ((sCurrentLine = br.readLine()) != null) {
+                            sCurrentLine1 = sCurrentLine;
+                        }
+                        bufferedWriter.write(textPwrBall + "\tPowerball\t" + dateFormat.format(date) + "\t" + sCurrentLine1 + "\n");
                         bufferedWriter.close();
 
                         System.out.println(textPwrBall);
@@ -165,7 +290,19 @@ public class Lottery {
             }
         }
         catch (Exception e) {
-            System.out.println("Error " + e.getMessage());
+            System.out.println("Error - incorrect answer please enter the number 1 or 2 ");
+        }
+        finally {
+            try {
+                if (br != null)
+                    br.close();
+
+                if (fr != null)
+                    fr.close();
+            }
+            catch (IOException ex) {
+                ex.printStackTrace();
+            }
         }
     }
 }
